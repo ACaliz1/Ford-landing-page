89047dd80585d81fa0662cd1e71fdde575183c90
acalizco <albertcaliz@gmail.com>
2024-08-17 20:55:00 +0200

    Landing page 95% done

--- script.js
+++ script.js
@@ -285,3 +285,17 @@ document.querySelectorAll('menu ul li').forEach(item => {
         item.classList.add('active');
     });
 });
+
+document.getElementById("legalInfo").addEventListener("click", function () {
+    let legalInfo = document.getElementById("legalInfo")
+    let legalInfoArrow = document.getElementById("legalInfo-arrow");
+    let isHidden = legalInfoArrow.classList.contains("hidden");
+
+    if (isHidden) {
+        legalInfoArrow.classList.remove("hidden");
+        legalInfo.innerHTML = `<strong>Información legal importante</strong><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#292929" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15l6 -6l6 6" /></svg>`;
+    } else {
+        legalInfoArrow.classList.add("hidden");
+        legalInfo.innerHTML = `<strong>Información legal importante</strong><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#292929" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-down"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 9l6 6l6 -6" /></svg>`;
+    }
+});
